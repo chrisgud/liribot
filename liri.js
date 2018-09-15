10c96f9900263bed4ac494db1598b51019e7eaff
Chris Gudmundsson <chrisgud85@gmail.com>
2018-09-15 12:41:16 -0500

    formatting and comments

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ const keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 const bandsInTown = keys.bandsInTown;
 const OMDB = keys.OMDB;
-
+//Parse command line args
 if (process.argv[2]) {
   let selection = process.argv[2];
   if (process.argv[3]) {
@@ -16,10 +16,39 @@ if (process.argv[2]) {
     process.argv.shift();
     var queryString = process.argv.join(' ');
   }
+  //call bot functionality
   selectOperation(selection, queryString);
-
 } else logResults('no arguments supplied');
-
+/**
+ * Directs by selection method calls with the Query string
+ * @param {string} selection
+ *  selection must be concert-this, spotify-this-song, movie-this, or do-what-it-says
+ * @param {string} queryString 
+ *  user provided search string for use with API call methods
+ */
+function selectOperation(selection, queryString) {
+  if (selection === 'concert-this') {
+    searchBandsInTown(queryString);
+  } else if (selection === 'spotify-this-song') {
+    searchSpotify(queryString);
+  } else if (selection === 'movie-this') {
+    searchOMDB(queryString);
+  } else if (selection === 'do-what-it-says') {
+    //import random.txt split on comma then call selectOperation with parsed information
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+      if (err) throw err;
+      let stuff = data.split(',');
+      let test = stuff[1].substring(stuff[1].indexOf('"') + 1, stuff[1].lastIndexOf('"'));
+      selectOperation(stuff[0], test);
+    });
+  }
+}
+/**
+ * searches the BandsInTown API with artist information.
+ * Displays upcoming event venues, locations and dates
+ * @param {string} artist 
+ *  search string for API call
+ */
 function searchBandsInTown(artist) {
   if (!artist) artist = 'Third Eye Blind';
   request(`https://rest.bandsintown.com/artists/${encodeURI(artist)}/events?app_id=codingbootcamp`,
@@ -37,23 +66,12 @@ function searchBandsInTown(artist) {
       }
     });
 }
-function selectOperation(selection, queryString) {
-  if (selection === 'concert-this') {
-    searchBandsInTown(queryString);
-  } else if (selection === 'spotify-this-song') {
-    searchSpotify(queryString);
-  } else if (selection === 'movie-this') {
-    searchOMDB(queryString);
-  } else if (selection === 'do-what-it-says') {
-    //import random.txt split on comma then call searchSpotifywith second element
-    fs.readFile('random.txt', 'utf8', function (err, data) {
-      if (err) throw err;
-      let stuff = data.split(',');
-      let test = stuff[1].substring(stuff[1].indexOf('"') + 1, stuff[1].lastIndexOf('"'));
-      selectOperation(stuff[0], test);
-    });
-  }
-}
+/**
+ * Searches the OMDB API for movie information by title search
+ * Displays various information received from the API
+ * @param {string} movie 
+ *  search string for API call
+ */
 function searchOMDB(movie) {
   if (!movie) movie = 'Mr. Nobody';
   request(`http://www.omdbapi.com/?t=${encodeURI(movie)}&y=&plot=short&apikey=${OMDB.id}`,
@@ -76,7 +94,12 @@ function searchOMDB(movie) {
       }
     });
 };
-
+/**
+ * Searches spotify using its node package by track using the song search string
+ * Displays Artist, Title, Album and provides a spotify preview url
+ * @param {string} song
+ *  Search term for API query
+ */
 function searchSpotify(song) {
   if (!song) song = 'The Sign Ace of Base';
   spotify.search({ type: 'track', query: song }, function (err, data) {
@@ -89,16 +112,14 @@ function searchSpotify(song) {
     logResults(`Album of Track: ${data.tracks.items[0].album.name}`);
   });
 }
-
+/**
+ * Displays content to the console log and writes it to log.txt
+ * @param {string} content
+ *  information to be logged
+ */
 function logResults(content) {
   fs.appendFile('log.txt', content + '\n', function (err) {
     if (err) throw err;
     console.log(content);
   });
 }
-/*
-### BONUS
-* In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-* Make sure you append each command you run to the `log.txt` file. 
-* Do not overwrite your file each time you run a command.
-  */
